330c487fe8724e579ce7daf75ff9fbfedabc9866
hamchick <chickenhamgod@gmail.com>
2023-05-11 15:59:17 +0800

    fixing gitignore

--- Model/Country.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PokemonReviewApp.Model
-{
-    public class Country
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public ICollection<Owner> owners { get; set; }
-    }
-}
